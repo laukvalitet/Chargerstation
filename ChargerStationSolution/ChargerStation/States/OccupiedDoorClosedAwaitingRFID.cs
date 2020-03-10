954662d3d78c19e87ef1dc04ce77b6d212228a81
Kristian Lau Jespersen <klj.jep@gmail.com>
2020-03-10 23:52:53 +0100

    Namespaces updated, states added

---
+++ ChargerStationSolution/ChargerStation/States/OccupiedDoorClosedAwaitingRFID.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargerStation.States
+{
+    class OccupiedDoorClosedAwaitingRFID : IState
+    {
+        public StationControl StationControlRef { get; set; }
+
+        public OccupiedDoorClosedAwaitingRFID(StationControl stationControlRef)
+        {
+            StationControlRef = stationControlRef;
+        }
+        public void OnDoorClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDoorOpened()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnRfidDetected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPhoneConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnPhoneDisconnected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ChargerStationSolution/ChargerStation/StationControl.cs
+++ ChargerStationSolution/ChargerStation/StationControl.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Ladeskab.States;
 
-namespace Ladeskab
+namespace ChargerStation
 {
     class StationControl
     {

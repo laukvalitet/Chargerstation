81c0a828465bfc11d12c5c14368ec5279f2f3157
oskar <oskar.vedel@gmail.com>
2020-03-23 17:24:41 +0100

    extended filelogger

--- ChargerStationSolution/ChargerStation/FileLogger.cs
+++ ChargerStationSolution/ChargerStation/FileLogger.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ChargerStation
 {
     public class FileLogger : ILogger
     {
+        public StreamWriter writer (string path, bool append);
+        
+        public FileLogger
+        {
+            writer = new StreamWriter ("ChargerstationLog.txt", true);
+        }
+        
         public void LogThis(string logText)
         {
             string time = DateTime.Now.ToString("h:mm:ss");
+            writer.Write("Logger at " +time+ " : "+logText);
         }
     }
 }

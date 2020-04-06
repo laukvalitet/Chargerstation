ccf566c1c7b254a87ba50a7e69770e59fd576bbf
laukvalitet <201807157@post.au.dk>
2020-04-06 18:48:19 +0200

    one more addition to third illegal state change test

--- ChargerStationSolution/ChargerStation.Test.Unit/IllegalStateChangeTests.cs
+++ ChargerStationSolution/ChargerStation.Test.Unit/IllegalStateChangeTests.cs
@@ -121,6 +121,7 @@ namespace ChargerStation.Test.Unit
             //act
             _doorSensor.DoorOpened += Raise.EventWith(EventArgs.Empty);
             _chargeControl.PhoneDisconnected += Raise.EventWith(EventArgs.Empty);
+            _chargeControl.PhoneConnected += Raise.EventWith(EventArgs.Empty);
             _rfidReader.RfidDetected += Raise.EventWith(new RfidDetectedEventArgs(2000));
 
 

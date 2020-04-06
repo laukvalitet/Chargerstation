7ef0635687d4ec9d2cea4f61a7125ec975ecea22
SBrahe <201807173@post.au.dk>
2020-04-06 15:06:36 +0200

    Rfidtest oprettet, ikke færdig

---
+++ ChargerStationSolution/ChargerStation.Test.Unit/RfidReaderTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using ChargerStation;
+
+namespace ChargerStation.Test.Unit
+{
+    [TestFixture]
+    class RfidReaderTest
+    {
+        private RfidReader _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut=new RfidReader();
+        }
+
+        [Test]
+        public void On_RfidDetected_invoke_event_Send_ID()
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+            
+        }
+
+    }
+}
